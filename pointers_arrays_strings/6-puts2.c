708d0f24d159d1fb5cd95c9c656686af3e02fcb7
munirahenad <muneraenad@hotmail.com>
2025-10-21 09:05:11 +0000

    fix

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,32 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+* puts2 - prints every other character of a string,
+* starting with the first character
+* @str: input string
+*
+* Description: This function prints only the characters
+* whose index numbers are even (0, 2, 4, ...).
+* The odd-indexed characters are skipped using an else statement.
+* After finishing, a newline character is printed.
+*
+* Return: void
+*/
 void puts2(char *str)
 {
-    int i;
+int i = 0;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (i % 2 == 0)
-            putchar(str[i]);
-    }
-    putchar('\n');
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+else
+{
+
+}
+i++;
+}
+_putchar('\n');
 }
