ef38adc3dcfafb7bae60b91142bc97cbe22a1749
munirahenad <muneraenad@hotmail.com>
2025-10-21 08:00:44 +0000

    fix

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -4,10 +4,8 @@
  * reset_to_98 - takes a pointer to an int and updates
  * the value it points to to 98
  * @n: pointer to an int
- *
- * Return: nothing
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
