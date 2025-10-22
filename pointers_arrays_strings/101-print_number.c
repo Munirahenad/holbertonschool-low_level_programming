d49ed1bd354630cc9318a68bc883f041b03e9064
munirahenad <muneraenad@hotmail.com>
2025-10-22 10:32:26 +0000

    fix

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,20 +1,27 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * print_number - prints an integer using only _putchar
+ * @n: integer to print
  */
 void print_number(int n)
 {
+    unsigned int m;
+
     if (n < 0)
     {
         _putchar('-');
-        n = -n;
+        /* convert to unsigned first, then negate to avoid INT_MIN overflow */
+        m = (unsigned int)(- (unsigned int)n);
+    }
+    else
+    {
+        m = (unsigned int)n;
     }
 
-    if (n / 10)
-        print_number(n / 10);
+    if (m / 10)
+        print_number((int)(m / 10));
 
-    _putchar((n % 10) + '0');
+    _putchar((char)((m % 10) + '0'));
 }
 
