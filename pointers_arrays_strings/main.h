0e4e57639b6933a9cb4f0b6b227be669f5769aa0
munirahenad <muneraenad@hotmail.com>
2025-10-27 08:44:11 +0000

    fix

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -14,6 +14,7 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 char *_memcpy(char *dest, char *src, unsigned int n);
 int _atoi(char *s);
+void set_string(char **s, char *to);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 #endif
