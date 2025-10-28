98a1ca4418d7ab32df782db56b4bae8dfe3c6c9d
munirahenad <muneraenad@hotmail.com>
2025-10-28 08:12:10 +0000

    add

--- recursion/main.h
+++ recursion/main.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_H
+i#ifndef MAIN_H
 #define MAIN_H
 
 void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
+int _pow_recursion(int x, int y);
 int _putchar(char c);
 
 #endif
