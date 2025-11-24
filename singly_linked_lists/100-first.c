901b3eb43fcd196c28e8a1c52e4825b691a40057
munirahenad <muneraenad@hotmail.com>
2025-11-24 11:06:15 +0000

    add

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -5,12 +5,12 @@ void print_before_main(void) __attribute__((constructor));
 /**
  * print_before_main - prints a message before main is executed
  *
- * This function is executed automatically before main
+ * This function will run automatically before main
  * because of the constructor attribute.
  */
 void print_before_main(void)
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
