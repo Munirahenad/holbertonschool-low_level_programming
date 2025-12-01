3400c1476a77f101b8b5c495fdb3db4d36726611
munirahenad <muneraenad@hotmail.com>
2025-12-01 09:05:51 +0000

    fix

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,25 +1,25 @@
 #include "lists.h"
 
 /**
-* free_dlistint - frees a dlistint_t list
+* get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
 * @head: pointer to the head of the list
+* @index: index of the node, starting from 0
 *
-* Description: Frees all nodes of a doubly linked list
+* Return: the nth node, or NULL if the node does not exist
 */
-void free_dlistint(dlistint_t *head)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-dlistint_t *current;
-dlistint_t *next_node;
-
-current = head;
+dlistint_t *current = head;
+unsigned int count = 0;
 
 while (current != NULL)
 {
-next_node = current->next;
+if (count == index)
+return (current);
 
-/* Free the current node */
-free(current);
-
-current = next_node;
+count++;
+current = current->next;
 }
+
+return (NULL);
 }
