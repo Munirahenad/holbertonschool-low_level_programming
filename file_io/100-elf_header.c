babfce4a318a73d71f86ec3bcb6e5498823b1973
munirahenad <muneraenad@hotmail.com>
2025-12-01 10:58:39 +0000

    add

--- file_io/100-elf_header.c
+++ file_io/100-elf_header.c
@@ -68,18 +68,11 @@ void print_data(unsigned char *e_ident)
 void print_version(unsigned char *e_ident)
 {
     printf("  Version:                           ");
-    switch (e_ident[EI_VERSION])
-    {
-    case EV_NONE:
-        printf("0 (invalid)\n");
-        break;
-    case EV_CURRENT:
-        printf("1 (current)\n");
-        break;
-    default:
-        printf("%d (unknown)\n", e_ident[EI_VERSION]);
-        break;
-    }
+    
+    if (e_ident[EI_VERSION] == EV_NONE)
+        printf("0\n");
+    else
+        printf("%d\n", e_ident[EI_VERSION]);
 }
 
 void print_osabi(unsigned char *e_ident)
